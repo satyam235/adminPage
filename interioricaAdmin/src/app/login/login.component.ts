64e6b1798bed37ad39b21c772cd8d2afed75636d
Satyam Shukla <satyamshukla95183@gmail.com>
2023-05-11 13:50:31 +0530

    a small fix

--- interioricaAdmin/src/app/login/login.component.ts
+++ interioricaAdmin/src/app/login/login.component.ts
@@ -9,17 +9,20 @@ import { environment } from 'src/environment/environment';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-
+import { ToastrService } from 'ngx-toastr';
 export class LoginComponent {
   email = "";
   password = "";
-  constructor() { }
+  constructor(private toastr: ToastrService) { }
 
   login() {
     if (this.email == environment.login_credentials.username && this.password == environment.login_credentials.password) {
       console.log("login success");
       localStorage.setItem('access_token', environment.login_credentials.access_token);
       window.location.href = "/admin";
+    }
+    else{
+      this.toastr.error('Invalid Credentials', 'Error');
     }
   }
 
