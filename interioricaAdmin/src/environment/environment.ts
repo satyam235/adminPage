b363b3f9bb756e173e75a396c9671b5ad064f482
Satyam Shukla <satyamshukla95183@gmail.com>
2023-05-11 13:47:48 +0530

    changed creds

--- interioricaAdmin/src/environment/environment.ts
+++ interioricaAdmin/src/environment/environment.ts
@@ -3,7 +3,8 @@ export const environment = {
     fetchData: `${host}`+'/admin/get-data',
     login_credentials : {
         username: 'admin',
-        password: 'admin',
-        access_token : 'admin'
+        password: 'Interiorica@9518',
+        access_token : 'XJQz8V7AfhjKoHv6Slm1gU0dtpkrxIy'
+        
     }
 }
